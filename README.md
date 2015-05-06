34458a45f510f59404a281f6a5ec1f0fc86467b5
Aben <tntaben@gmail.com>
2015-05-06 18:19:34 +0800

    add function: splitDeps

--- README.md
+++ README.md
@@ -1,7 +1,41 @@
 # Crisper
-> Split inline scripts from an HTML file for CSP compliance
+> Split scripts and styles from an HTML file for CSP compliance
+> Add attribute `landing` will keep it in HTML file 
 
 ## Usage
 
-    cat index.html | crisper -h build.html -j build.js
-    crisper --source index.html --html build.html --js build.js
+    cat index.html | crisper -h build.html -j build.js -c build.css
+    crisper --source index.html --html build.html --js build.js --css build.css
+
+or
+
+    crisper --source index.html --deps build --all
+
+## API
+
+### split(source, jsFileName[, cssFileName])
+
+Split inline scripts and styles, return an object.
+
+    {
+      html: 'html string',
+      js: 'js string',
+      style: 'style string'
+    }
+
+### splitDeps(source, all=false)
+
+Split inline scripts and inline styles return string, parse external scripts and external styles return path.
+`all=false` that will fail through a file with uri, like `https://code.jquery.com/jquery-2.1.4.min.js`,
+set `true` pass through. lower than the `landing` attribute.
+  
+    {
+      html: 'html string'
+      js: 'inline js string'
+      style: 'inline style string'
+      deps: {
+        js: ['js/foo.js', 'js/bar.js']
+        css: ['css/common.css']
+      }
+    }
+
--- index.js
+++ index.js
@@ -24,16 +24,56 @@ var inlineScriptFinder = pred.AND(
   ),
   pred.NOT(
     pred.hasAttr('src')
+  ),
+  pred.NOT(
+    pred.hasAttr('landing')
   )
 );
 
-function split(source, jsFileName) {
+var inlineStyleFinder = pred.AND(
+  pred.hasTagName('style'),
+  pred.OR(
+    pred.NOT(
+      pred.hasAttr('type')
+    ),
+    pred.hasAttrValue('type', 'text/css')
+  ),
+  pred.NOT(
+    pred.hasAttr('landing')
+  )
+);
+
+var externalScriptFinder = pred.AND(
+  pred.hasTagName('script'),
+  pred.hasAttr('src'),
+  pred.OR(
+    pred.NOT(
+      pred.hasAttr('type')
+    ),
+    pred.hasAttrValue('type', 'text/javascript'),
+    pred.hasAttrValue('type', 'application/javascript')
+  ),
+  pred.NOT(
+    pred.hasAttr('landing')
+  )
+);
+
+var externalStyleFinder = pred.AND(
+  pred.hasTagName('link'),
+  pred.hasAttrValue('rel', 'stylesheet'),
+  pred.NOT(
+    pred.hasAttr('landing')
+  )
+);
+
+function split(source, jsFileName, cssFileName) {
   var doc = dom5.parse(source);
   var head = dom5.query(doc, pred.hasTagName('head'));
   var body = dom5.query(doc, pred.hasTagName('body'));
   var scripts = dom5.queryAll(doc, inlineScriptFinder);
+  var styles = dom5.queryAll(doc, inlineStyleFinder);
 
-  var contents = [];
+  var jsContents = [];
   scripts.forEach(function(sn) {
     var nidx = sn.parentNode.childNodes.indexOf(sn) + 1;
     var next = sn.parentNode.childNodes[nidx];
@@ -42,23 +82,139 @@ function split(source, jsFileName) {
     if (next && dom5.isTextNode(next) && !/\S/.test(dom5.getTextContent(next))) {
       dom5.remove(next);
     }
-    contents.push(dom5.getTextContent(sn));
+    jsContents.push(dom5.getTextContent(sn));
   });
 
   var newScript = dom5.constructors.element('script');
   dom5.setAttribute(newScript, 'src', jsFileName);
   dom5.append(body, newScript);
 
+  var styleContents = [];
+  if (cssFileName) {
+    styles.forEach(function(sn){
+      var nidx = sn.parentNode.childNodes.indexOf(sn) + 1;
+      var next = sn.parentNode.childNodes[nidx];
+      dom5.remove(sn);
+      // remove newline after style to get rid of nasty whitespace
+      if (next && dom5.isTextNode(next) && !/\S/.test(dom5.getTextContent(next))) {
+        dom5.remove(next);
+      }
+      styleContents.push(dom5.getTextContent(sn));
+    })
+
+    var newCss = dom5.constructors.element('link');
+    dom5.setAttribute(newCss, 'rel', 'stylesheet');
+    dom5.setAttribute(newCss, 'href', cssFileName);
+    dom5.append(head, newCss);
+  }
+
   var html = dom5.serialize(doc);
   // newline + semicolon should be enough to capture all cases of concat
-  var js = contents.join('\n;');
+  var js = jsContents.join('\n;');
+  var style = styleContents.join('\n');
 
   return {
     html: html,
-    js: js
+    js: js,
+    style: style
+  };
+}
+
+function splitDeps(source, all) {
+  var doc = dom5.parse(source);
+  var head = dom5.query(doc, pred.hasTagName('head'));
+  var body = dom5.query(doc, pred.hasTagName('body'));
+  var scripts = dom5.queryAll(doc, inlineScriptFinder);
+  var styles = dom5.queryAll(doc, inlineStyleFinder);
+  var jsFiles = dom5.queryAll(doc, externalScriptFinder);
+  var cssFiles = dom5.queryAll(doc, externalStyleFinder);
+
+  // split inline script
+  var jsContents = [];
+  scripts.forEach(function(sn) {
+    var nidx = sn.parentNode.childNodes.indexOf(sn) + 1;
+    var next = sn.parentNode.childNodes[nidx];
+    dom5.remove(sn);
+    // remove newline after script to get rid of nasty whitespace
+    if (next && dom5.isTextNode(next) && !/\S/.test(dom5.getTextContent(next))) {
+      dom5.remove(next);
+    }
+    jsContents.push(dom5.getTextContent(sn));
+  });
+
+  // split inline style
+  var styleContents = [];
+  styles.forEach(function(sn){
+    var nidx = sn.parentNode.childNodes.indexOf(sn) + 1;
+    var next = sn.parentNode.childNodes[nidx];
+    dom5.remove(sn);
+    // remove newline after style to get rid of nasty whitespace
+    if (next && dom5.isTextNode(next) && !/\S/.test(dom5.getTextContent(next))) {
+      dom5.remove(next);
+    }
+    styleContents.push(dom5.getTextContent(sn));
+  })
+
+  // parse external js and css, return a list
+  var deps = {
+    js: [],
+    css: []
+  }
+  if (!all) {
+    jsFiles = jsFiles.filter(function(sn){
+      // filter third part js file
+      if(dom5.getAttribute(sn, 'src').indexOf('://') > -1) {
+        return false
+      } else {
+        return true
+      }
+    })
+  }
+  jsFiles.forEach(function(sn){
+    var nidx = sn.parentNode.childNodes.indexOf(sn) + 1;
+    var next = sn.parentNode.childNodes[nidx];
+    dom5.remove(sn);
+    // remove newline after style to get rid of nasty whitespace
+    if (next && dom5.isTextNode(next) && !/\S/.test(dom5.getTextContent(next))) {
+      dom5.remove(next);
+    }
+    deps.js.push(dom5.getAttribute(sn, 'src'))
+  })
+  if (!all) {
+    cssFiles = cssFiles.filter(function(sn){
+      if(dom5.getAttribute(sn, 'href').indexOf('://') > -1) {
+        return false
+      } else {
+        return true
+      }
+    })
+  }
+  cssFiles.forEach(function(sn){
+    var nidx = sn.parentNode.childNodes.indexOf(sn) + 1;
+    var next = sn.parentNode.childNodes[nidx];
+    dom5.remove(sn);
+    // remove newline after style to get rid of nasty whitespace
+    if (next && dom5.isTextNode(next) && !/\S/.test(dom5.getTextContent(next))) {
+      dom5.remove(next);
+    }
+    deps.css.push(dom5.getAttribute(sn, 'href'))
+  })
+
+
+  var html = dom5.serialize(doc);
+  // newline + semicolon should be enough to capture all cases of concat
+  var js = jsContents.join('\n;');
+  var style = styleContents.join('\n');
+
+  return {
+    html: html,
+    js: js,
+    style: style,
+    deps: deps
   };
 }
 
 module.exports = {
-  split: split
+  split: split,
+  splitDeps: splitDeps
 };
